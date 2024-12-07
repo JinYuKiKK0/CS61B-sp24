b3293558c250c97132d9c91a27d711ec37ae70bc
JinYuK1KK0 <2926900673@qq.com>
2024-12-07 22:41:25 +0800

    12/7 22:41

--- proj2a/src/ngrams/NGramMap.java
+++ proj2a/src/ngrams/NGramMap.java
@@ -5,6 +5,7 @@ import edu.princeton.cs.algs4.In;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 import static ngrams.TimeSeries.MAX_YEAR;
 import static ngrams.TimeSeries.MIN_YEAR;
@@ -22,15 +23,36 @@ import static ngrams.TimeSeries.MIN_YEAR;
 public class NGramMap {
 
     // TODO: Add any necessary static/instance variables.
-    HashMap<String,TimeSeries> words;
-    ArrayList<TimeSeries> counts;
+    HashMap<String,TimeSeries> wordsMap;//存储word与时间序列的映射
+    ArrayList<TimeSeries> countsMap;//存储各个年份的总卷数
     /**
      * Constructs an NGramMap from WORDSFILENAME and COUNTSFILENAME.
      */
     public NGramMap(String wordsFilename, String countsFilename) {
         // TODO: Fill in this constructor. See the "NGramMap Tips" section of the spec for help.
+        wordsMap = new HashMap<>();
+        countsMap = new ArrayList<>();
         In words = new In(wordsFilename);
+        while(words.hasNextLine()){
+            String readLine = words.readLine();
+            String[] wordsSplit = readLine.split("\t");
+            String word = wordsSplit[0];
+            String year = wordsSplit[1];
+            String times = wordsSplit[2];
+            TimeSeries wordTs = new TimeSeries();//每次循环都重新创建TS，没有保存
+            wordTs.put(Integer.parseInt(year),Double.parseDouble(times));
+            wordsMap.put(word,wordTs);
+        }
         In counts = new In(countsFilename);
+        while(counts.hasNextLine()){
+            String readLine = counts.readLine();
+            String[] countsSplit = readLine.split(",");
+            String year = countsSplit[0];
+            String count = countsSplit[1];
+            TimeSeries countTs =new TimeSeries();
+            countTs.put(Integer.parseInt(year),Double.parseDouble(count));
+            countsMap.add(countTs);
+        }
     }
 
     /**
@@ -46,7 +68,11 @@ public class NGramMap {
      */
     public TimeSeries countHistory(String word, int startYear, int endYear) {
         // TODO: Fill in this method.
-        return null;
+        if(wordsMap.containsKey(word)){
+            TimeSeries wordSeries = wordsMap.get(word);
+            TimeSeries ts = new TimeSeries(wordSeries,startYear,endYear);
+            return ts;
+        }else return new TimeSeries();
     }
 
     /**
@@ -57,7 +83,11 @@ public class NGramMap {
      */
     public TimeSeries countHistory(String word) {
         // TODO: Fill in this method.
-        return null;
+        if(wordsMap.containsKey(word)){
+            TimeSeries wordSeries = wordsMap.get(word);
+            List<Integer> years = wordSeries.years();
+            return new TimeSeries(wordSeries, years.get(0), years.get(years.size()-1));
+        }else return new TimeSeries();
     }
 
     /**
@@ -99,8 +129,7 @@ public class NGramMap {
      * 提供 WORDS 中所有词语在 STARTYEAR 至ENDYEAR，包含两端 之间每年总的相对频率。
      * 如果某个词不在该时间范围内，则忽略它
      */
-    public TimeSeries summedWeightHistory(Collection<String> words,
-                                          int startYear, int endYear) {
+    public TimeSeries summedWeightHistory(Collection<String> words, int startYear, int endYear) {
         // TODO: Fill in this method.
         return null;
     }
@@ -115,5 +144,6 @@ public class NGramMap {
     }
 
     // TODO: Add any private helper methods.
+
     // TODO: Remove all TODO comments before submitting.
 }
